d2bfdb9b6e2516dea74743ce4aa6ff414fb36105
James Hardy <cyanjamesmail@gmail.com>
2023-07-07 01:13:44 +0100

    Completion of Menu MVC

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -7,8 +7,39 @@ use dioxus::prelude::*;
 pub fn App(cx: Scope) -> Element {
     cx.render(rsx! {
         div {
+            style { include_str!("./style.css") }
             main {
-                "ImageThreader"
+                div { class: "main-wrapper",
+                    div { class: "menu-row-grid",
+                        div { class: "menu-grid-item menu-header",
+                            img {
+                                src: "./src/assets/menu-title-logo.svg"
+
+                            }
+                        },
+                        div { class: "menu-grid-item",
+                            div { class: "menu-content-button",     
+                                img {src: "./src/assets/menu-image.svg", alt: "Convert from Image"}
+                                div { class: "menu-content-button-text",
+                                    "Convert from Image"
+                                }
+                            },
+                            div { class: "menu-content-button",     
+                                img {src: "./src/assets/menu-image.svg"}
+                                div { class: "menu-content-button-text",
+                                    "View Converted Image"
+                                }
+                            },
+                        },
+                        div { class: "menu-grid-item menu-footer",
+                            div { class: "menu-footer-text",
+                                "YammyToast, 2023"
+                            }
+
+                        }
+
+                    }
+                }
             }
         }
     })
